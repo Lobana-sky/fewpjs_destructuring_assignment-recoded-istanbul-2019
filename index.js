f593666b770ddba6c86caa5bc97f274fe9a16c76
lobana alarabi <loubanaeng@gmail.com>
2020-02-02 14:59:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ const k_muppet = {
   k_job: 'Host of The Muppet Show',
   k_partner: 'Miss Piggy'
 };
-const {k_name,k_color,song_2,song_4,k_job,k_partner}= k_muppet.k_album.the_muppet_movie;
-console.log(k_color,song_2,song_4,k_job,k_partner);
+const {song_2,song_4}= k_muppet.k_album.the_muppet_movie;
+console.log(song_2,song_4);
 
 const {k_name,k_color,k_job,k_partner}= k_muppet;
 console.log(k_name,k_color,k_job,k_partner);
